263330ed3d93f4ea8e7dfbe156762e01fe1266d6
harshbhadoria011 <harshbhadoria011@gmail.com>
2024-03-29 19:47:47 +0530

    Added script to add two numbers

---
+++ AddNumber.cs
@@ -0,0 +1,9 @@
+namespace AddNumbers{
+    public class Program{
+        public static void Main(string[] args){
+            int a=5;
+            int b=10;
+            Console.WriteLine(a+b);
+        }
+    }
+}
